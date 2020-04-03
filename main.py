3bd6c642ebea2b619f17606cee6cc402ed64f854
Gagan <gagandeep231020@gmail.com>
2020-04-03 14:55:23 +0530

    Added duplicate checking

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ engine.connect()
 Session = sessionmaker(bind = engine)
 session = Session()
 app = FastAPI()
+d=0.005
 class Pla(BaseModel):
     lat:float
-    lon:float
+    lng:float
     pin:str
     address:str
     city:str
@@ -21,7 +22,39 @@ class Pla(BaseModel):
 
 @app.post('/post_location')
 async def create_place(place : Pla):
-    p=schemas.Places(key=place.pin,admin_name1=place.city,latitude=place.lat,longitude=place.lon,place_name=place.address)
+   
+    res=session.query(schemas.Places).\
+        filter(schemas.Places.key==place.pin)
+    res2=session.query(schemas.Places).\
+        filter(\
+        schemas.Places.latitude>place.lat-d,\
+        schemas.Places.latitude<place.lat+d,\
+        schemas.Places.longitude>place.lng-d,\
+        schemas.Places.longitude<place.lng+d )
+    if(res.count()!=0):
+        return "Pin already exists"
+    if(res2.count()!=0):
+        return "Location already exists"
+    p=schemas.Places(key=place.pin,admin_name1=place.city,latitude=place.lat,longitude=place.lng,place_name=place.address)
     session.add(p)
-    session.commit()
-    return 1
+    try:
+        session.commit()
+    except:
+        return "Some Error Occured"
+    return "Added Succesfully"
+
+@app.get('/get_location')
+async def getrow(lat:float,lng:float):
+    res=session.query(schemas.Places).\
+        filter(schemas.Places.latitude.in_([lat-d,lat+d])\
+            ,schemas.Places.longitude.in_([lng-d,lng+d]))
+    if(res.count()==0):
+        return "not found"
+    else:
+        ans={}
+        l=res.all()[0]
+        ans['pin_code']=l.key
+        ans['city']=l.admin_name1
+        ans['address']=l.place_name
+        return ans
+
